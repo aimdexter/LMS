953f66ff3b8f506284eeec35b738adddf2a953c8
aimdexter <draimade@gmail.com>
2023-12-08 00:09:01 +0100

    feat: updated data

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { CheckCircle, Clock } from "lucide-react";
 
@@ -14,29 +14,26 @@ export default async function Dashboard() {
     return redirect("/");
   }
 
-  const {
-    completedCourses,
-    coursesInProgress
-  } = await getDashboardCourses(userId);
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    userId
+  );
 
   return (
     <div className="p-6 space-y-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-       <InfoCard
+        <InfoCard
           icon={Clock}
-          label="In Progress"
+          label="En cours"
           numberOfItems={coursesInProgress.length}
-       />
-       <InfoCard
+        />
+        <InfoCard
           icon={CheckCircle}
-          label="Completed"
+          label="Complété"
           numberOfItems={completedCourses.length}
           variant="success"
-       />
+        />
       </div>
-      <CoursesList
-        items={[...coursesInProgress, ...completedCourses]}
-      />
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
     </div>
-  )
+  );
 }

ceb402deec8e61b0a62749dc0763ce727c004a3a
aimdexter <draimade@gmail.com>
2024-01-25 21:06:24 +0100

    feat: getting courses based on similarity

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -7,17 +7,16 @@ import { getCourses } from "@/actions/get-courses";
 import { CoursesList } from "@/components/courses-list";
 
 import { Categories } from "./_components/categories";
+import { openai } from "@/lib/openai";
 
 interface SearchPageProps {
   searchParams: {
     title: string;
     categoryId: string;
-  }
-};
+  };
+}
 
-const SearchPage = async ({
-  searchParams
-}: SearchPageProps) => {
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
   const { userId } = auth();
 
   if (!userId) {
@@ -26,8 +25,8 @@ const SearchPage = async ({
 
   const categories = await db.category.findMany({
     orderBy: {
-      name: "asc"
-    }
+      name: "asc",
+    },
   });
 
   const courses = await getCourses({
@@ -35,19 +34,43 @@ const SearchPage = async ({
     ...searchParams,
   });
 
+  if (searchParams.title) {
+    const embedding = await generateEmbedding(searchParams.title);
+    const vectorQuery = `[${embedding.join(",")}]`;
+    const similarCourses = await db.$queryRaw`
+      SELECT
+        id,
+        1 - (vector <=> ${vectorQuery}::vector) as similarity
+      FROM "Course"
+      where 1 - (vector <=> ${vectorQuery}::vector) > .5
+      ORDER BY  similarity DESC;
+    `;
+
+    console.log(similarCourses);
+  }
+
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
       <div className="p-6 space-y-4">
-        <Categories
-          items={categories}
-        />
+        <Categories items={categories} />
         <CoursesList items={courses} />
       </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default SearchPage;
+
+async function generateEmbedding(raw: string) {
+  // OpenAI recommends replacing newlines with spaces for best results
+  const input = raw.replace(/\n/g, " ");
+  const embeddingData = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input,
+  });
+  const [{ embedding }] = (embeddingData as any).data;
+  return embedding;
+}
